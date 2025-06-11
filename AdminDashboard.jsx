73bbc10959054bf624673e263febda9535ab7d5c
Justus <justus18860@gmail.com>
2025-06-11 16:13:48 +0300

    Create AdminDashboard.jsx

---
+++ AdminDashboard.jsx
@@ -0,0 +1,12 @@
+// AdminDashboard.jsx
+import React from 'react';
+
+function AdminDashboard() {
+  return (
+    <div>
+      <h1>Admin Dashboard Loaded Successfully</h1>
+    </div>
+  );
+}
+
+export default AdminDashboard;
